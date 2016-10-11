8a9d6fc9190a72f6fd0101f63aac9926ecfd94f8
Will Murphy <william@datatitian.com>
2016-10-11 18:00:40 -0500

    licensing and CRAN submission

---
+++ cran-comments.md
@@ -0,0 +1,19 @@
+## Test environments
+* local Win10 install, R 3.3.1
+* ubuntu 12.04 (on travis-ci), R 3.3.1
+* win-builder (devel)
+
+## R CMD check results
+
+0 errors | 0 warnings | 1 note
+
+CRAN incoming feasibility
+ * This is a new submission
+ * Version contains large components 
+ * Flagged words are spelled as intended:
+      - choropleths (correct)
+      - ggplot (package name)
+
+## Reverse dependencies
+
+This is a new release, so there are no reverse dependencies.
--- data-raw/make_mapfile.R
+++ data-raw/make_mapfile.R
@@ -1,3 +1,17 @@
+# This file is part of fiftystater.
+#
+# fiftytater is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# fiftystater is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with fiftystater  If not, see <http://www.gnu.org/licenses/>.
+
 # Create map data with AK, HI inset.
 # Inspired by http://stackoverflow.com/a/13767984
 library(rgeos)
